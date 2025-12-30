8ec1753023cd99c0fc7f8490eba785c827f52779
Sean <keen.coffee2501@fourteen10.com>
2025-12-29 20:05:10 -0800

    Refactor email generation to use async random word fetching

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,19 +1,21 @@
 const emailStore = require('../../store');
 
-function randomWord() {
-  return Math.random().toString(36).substring(2, 7);
+async function fetchRandomWord() {
+  const response = await fetch('https://random-word-api.herokuapp.com/word');
+  const word = await response.text();
+  return word;
 }
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   const { duration } = req.body;
-  const word1 = randomWord();
-  const word2 = randomWord();
+  const word1 = await fetchRandomWord();
+  const word2 = await fetchRandomWord();
   const digits = Math.floor(Math.random() * 10000).toString().padStart(4, '0');
-  const email = `${word1}.${word2}.${digits}@mailvoid.win`;
+  const email = `${word1}.${word2}${digits}@mailvoid.win`;
 
   const expiration = duration === 'unlimited' ? null : Date.now() + 10 * 60 * 1000;
 
